f886a06f5cf296fa027491bbb38f42098115438a
davidedavoli <davide.davoli4@studio.unibo.it>
2018-06-01 22:34:38 +0200

    La freccia funziona

--- src/algraphPackage/Arrow.java
+++ src/algraphPackage/Arrow.java
@@ -34,6 +34,8 @@ public class Arrow {
 	}
 	
 	public void managePointer() {
+		boolean segno2;
+		boolean segno3;
 		line2.setEndX(line1.getEndX());
 		line2.setEndY(line1.getEndY());
 		line3.setEndX(line1.getEndX());
@@ -44,15 +46,49 @@ public class Arrow {
 		double angle2 = angle + Math.PI/4;
 		double angle3 = angle - Math.PI/4;
 		
-		System.out.println(Math.atan(tanTeta));
-		
 		double tanTeta2 = Math.tan(angle2);
+		double tanTeta3 = Math.tan(angle3);
+		
+		if (tanTeta<1 && tanTeta>-1) {
+			if (line1.getStartX()<line1.getEndX()) {
+				
+				segno2=false;
+				segno3=false;
+			}
+			else {
+				segno2=true;
+				segno3=true;
+			}
+		}
+		else {
+				if (line1.getStartY()>line1.getEndY()) {
+					segno2=false;
+					segno3=true;
+				}
+				
+				else {
+					segno2=true;
+					segno3=false;
+				}
+		}
+		if (!segno2) {
 		line2.setStartX(line2.getEndX()-(10/Math.sqrt(Math.pow(tanTeta2, 2)+1)));
 		line2.setStartY(line2.getEndY()-(10/Math.sqrt(Math.pow(tanTeta2, 2)+1))*tanTeta2);
-		
-		double tanTeta3 = Math.tan(angle3);
-		line3.setStartX(line3.getEndX()-(10/Math.sqrt(Math.pow(tanTeta3, 2)+1)));
-		line3.setStartY(line3.getEndY()-(10/Math.sqrt(Math.pow(tanTeta3, 2)+1))*tanTeta3);
+		}
+		if(segno2) {
+			line2.setStartX(line2.getEndX()+(10/Math.sqrt(Math.pow(tanTeta2, 2)+1)));
+			line2.setStartY(line2.getEndY()+(10/Math.sqrt(Math.pow(tanTeta2, 2)+1))*tanTeta2);
+		}
+		if(!segno3) {
+			line3.setStartX(line3.getEndX()-(10/Math.sqrt(Math.pow(tanTeta3, 2)+1)));
+			line3.setStartY(line3.getEndY()-(10/Math.sqrt(Math.pow(tanTeta3, 2)+1))*tanTeta3);
 	}
+		if (segno3) {
+			line3.setStartX(line3.getEndX()+(10/Math.sqrt(Math.pow(tanTeta3, 2)+1)));
+			line3.setStartY(line3.getEndY()+(10/Math.sqrt(Math.pow(tanTeta3, 2)+1))*tanTeta3);
+		}
+	}
+	
+	
 	
 }
