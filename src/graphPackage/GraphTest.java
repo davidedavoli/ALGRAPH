f964e6ec609f49b6e5c9da14ab7e992a409702a3
davidedavoli <davide.davoli4@studio.unibo.it>
2018-06-17 10:58:07 +0200

    Aggiunto metodo per la ricerca di cicli negativi

--- src/graphPackage/GraphTest.java
+++ src/graphPackage/GraphTest.java
@@ -1,5 +1,9 @@
 package graphPackage;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class GraphTest {
 
 	public static void main(String[] args) {
@@ -7,6 +11,8 @@ public class GraphTest {
 		GraphVisit<String> Positano=new GraphVisit<String>();
 		Tudor.randomGraph(10, 2, 10, -10, true);
 		Tudor.print();
+		LinkedList<Node<String>> t=(LinkedList<Node<String>>) Positano.detectNegativeCycles(Tudor);
+		System.out.println(t.toString());
 		Positano.BellmanFord(Tudor, true);
 		Tudor.outGraph("src/tudor.txt");
 		Graph<String> Denis=new Graph<String>();
--- src/graphPackage/GraphVisit.java
+++ src/graphPackage/GraphVisit.java
@@ -61,4 +61,64 @@ public class GraphVisit<T extends Comparable<T>> {
 		}
 		// GraphVisualize(G, q, n, m, distances, parents);
 	}
+	
+	public List<Node<T>> detectNegativeCycles(Graph<T> G) {
+		List<Node<T>> cycleList=new LinkedList<Node<T>>();
+		int i=0;
+		TreeMap<Node<T>, Integer> index=new TreeMap<Node<T>, Integer>();
+		for(Node<T> n : G.V()) {
+			index.put(n, i);
+			i=i+1;
+		}
+		i=0;
+		
+		Integer[] distances= new Integer[G.V().size()];
+		for (Node<T> n : G.V())
+			distances[index.get(n)]=null;
+		
+		
+		Node<T>[] parents= new Node[G.V().size()];
+		for (Node<T> n : G.V())
+			parents[index.get(n)]=null;
+		
+		Node<T> radice=G.getRoot(0);
+		
+		distances[index.get(radice)]=0;
+		
+		for(int i1=0; i1<G.V().size()-2;i1++) {
+			for(Node<T> n: G.V()) {
+				for (Node<T> m: G.adj(n)) {
+					if (distances[index.get(n)]!=null && (distances[index.get(m)]==null || distances[index.get(n)]+G.w(n, m)<distances[index.get(m)])) {			
+						parents[index.get(m)]=n;
+						distances[index.get(m)]=distances[index.get(n)]+G.w(n, m);
+					}	
+				}
+		}
+	}
+		
+		Node<T> errorNode=null;
+			for(Node<T> n: G.V()) {
+				for (Node<T> m: G.adj(n)) {
+				if (distances[index.get(m)]!=null && distances[index.get(n)]!=null && distances[index.get(m)]>distances[index.get(n)]+G.w(n, m)) {
+					errorNode=m;
+					cycleList.add(errorNode);
+					}
+				if (errorNode!=null)
+					break;
+				}
+			if (errorNode!=null)
+				break;
+			}
+		Node<T> q=null;
+		if (errorNode!=null) {
+			q=parents[index.get(errorNode)];
+			while (!cycleList.contains(q)) {
+				cycleList.add(q);
+				q=parents[index.get(q)];
+			}
+			cycleList.add(errorNode);
+			//cycleList.add(parents[index.get(parents[index.get(p)])]);
+		}
+		return cycleList;
+	}
 }
